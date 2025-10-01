e4e79c7cde8ec4aab140b296ceaad220dd41094c
Meiqi Wen <meiqi.m.wen@outlook.com>
2025-10-01 14:36:16 -0500

    v4

--- src/components/GalleryView.tsx
+++ src/components/GalleryView.tsx
@@ -121,31 +121,6 @@ const GalleryView: React.FC = () => {
     setSelectedType(typeName);
   };
 
-
-  const getTypeColor = (typeName: string): string => {
-    const colors: { [key: string]: string } = {
-      normal: '#A8A878',
-      fire: '#F08030',
-      water: '#6890F0',
-      electric: '#F8D030',
-      grass: '#78C850',
-      ice: '#98D8D8',
-      fighting: '#C03028',
-      poison: '#A040A0',
-      ground: '#E0C068',
-      flying: '#A890F0',
-      psychic: '#F85888',
-      bug: '#A8B820',
-      rock: '#B8A038',
-      ghost: '#705898',
-      dragon: '#7038F8',
-      dark: '#705848',
-      steel: '#B8B8D0',
-      fairy: '#EE99AC'
-    };
-    return colors[typeName] || '#68A090';
-  };
-
   const getFallbackSprite = (pokemon: Pokemon): string => {
     // Try multiple sprite sources first
     const spriteSources = [
